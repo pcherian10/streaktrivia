a1654021b00f024a4fe6a527f3f749afba5825fa
pcherian10 <pcherian10@gmail.com>
2019-05-18 17:15:35 -0400

    installed updated versions of redux and and react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux' 
+
+
 import App from './components/App';
 
-ReactDOM.render(<App />, 
+const store = createStore(() => [], {}, applyMiddleware() );
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
 
