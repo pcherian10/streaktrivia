7d565bbdc4a053ade8e3566af04c4d18e99e4043
pcherian10 <pcherian10@gmail.com>
2019-05-18 20:43:11 -0400

    set up intial routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Header = () => <h2>Header</h2>
+const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const PlayGame = () => <h2>Game</h2>
+const SubmitAQuestion = () => <h2>Submit a Question</h2>
+
 
 const App = () => {
     return (
         <div>
-            Hi There!
+            <BrowserRouter>
+                <div>
+                    <Route exact path="/" component={Landing}/>
+                    <Route path="/rankings" component={Dashboard}/>
+                    <Route path="/games/new" component={PlayGame}/>
+                    <Route path="/questions/new" component={SubmitAQuestion}/>
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
