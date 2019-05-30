ca47ba48d2c73c1e29ea946de33e5b42228c9deb
pcherian10 <pcherian10@gmail.com>
2019-05-30 17:47:37 -0400

    completed the landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Dropdown, Message, Reveal, Image } from 'semantic-ui-react'
 import './App.css'
 
 
@@ -10,14 +10,32 @@ const Landing = () => {
             <div className="question-mark">
                 <Icon style={{ color: "orange", fontSize: '300px'}} name="question circle"></Icon>
             </div>
-            <div class="title">
+            <div className="title">
                 STREAK
             </div>
-            <div class="subtitle">
+            <div className="subtitle">
                 TRIVIA
             </div>
-            <div>
+            <div id="intro">
+                A simple, crowd-sourced trivia game 
+                <br></br>
             </div>
+            <div class="dropdown">
+                <span>Instructions</span>
+                <div class="dropdown-content">
+                    <p> Answer questions created by your fellow earthlings. 
+                        Earn the longest string of correct
+                        answers and you will be made the ruler of all things. 
+                        Just kidding, (maybe). <br></br><br></br>
+                        You can also write questions that will be shown to
+                        other players, to trip them up
+                        <span style={{ fontSize:'17px' }}> &#128527;</span> or
+                        to teach them something new. 
+                        <span style={{ fontSize:'17px' }}> &#129299;</span>
+                    </p>
+                </div>
+            </div>
+            
         </div>
     )
 }
